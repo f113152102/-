b2b64a1642279b685f909b7569d76181f84c54f9
f113152102 <f113152102@nkust.edu.tw>
2024-08-04 16:32:47 +0800

    Update 0802arXivAPI2.py

--- .github/0802arXivAPI2.py
+++ .github/0802arXivAPI2.py
@@ -3,51 +3,51 @@ import requests
 import json
 import arxiv
 import os
-
-def get_authors(authors, first_author=False):
+def get_authors(authors, first_author = False):
     output = str()
     if first_author == False:
         output = ", ".join(str(author) for author in authors)
     else:
         output = authors[0]
     return output
-
 def sort_papers(papers):
     output = dict()
     keys = list(papers.keys())
     keys.sort(reverse=True)
     for key in keys:
         output[key] = papers[key]
-    return output
-
-def get_daily_papers(topic, query="slam", max_results=2):
+    return output    
+ 
+def get_daily_papers(topic,query="slam", max_results=2):
     """
     @param topic: str
     @param query: str
     @return paper_with_code: dict
     """
-
+ 
     # output 
     content = dict() 
     
     search_engine = arxiv.Search(
-        query=query,
-        max_results=max_results,
-        sort_by=arxiv.SortCriterion.SubmittedDate
+        query = query,
+        max_results = max_results,
+        sort_by = arxiv.SortCriterion.SubmittedDate
     )
  
     for result in search_engine.results():
  
-        paper_id = result.get_short_id()
-        paper_title = result.title
-        paper_url = result.entry_id
-        paper_abstract = result.summary.replace("\n", " ")
-        paper_authors = get_authors(result.authors)
-        paper_first_author = get_authors(result.authors, first_author=True)
+        paper_id       = result.get_short_id()
+        paper_title    = result.title
+        paper_url      = result.entry_id
+ 
+        paper_abstract = result.summary.replace("\n"," ")
+        paper_authors  = get_authors(result.authors)
+        paper_first_author = get_authors(result.authors,first_author = True)
         primary_category = result.primary_category
+ 
         publish_time = result.published.date()
  
-        print("Time = ", publish_time,
+        print("Time = ", publish_time ,
               " title = ", paper_title,
               " author = ", paper_first_author)
  
@@ -56,22 +56,22 @@ def get_daily_papers(topic, query="slam", max_results=2):
         if ver_pos == -1:
             paper_key = paper_id
         else:
-            paper_key = paper_id[0:ver_pos]
+            paper_key = paper_id[0:ver_pos] 
  
         content[paper_key] = f"|**{publish_time}**|**{paper_title}**|{paper_first_author} et.al.|[{paper_id}]({paper_url})|\n"
-    data = {topic: content}
+    data = {topic:content}
     
-    return data
+    return data 
  
-def update_json_file(filename, data_all):
-    with open(filename, "r", encoding='utf-8') as f:
+def update_json_file(filename,data_all):
+    with open(filename,"r") as f:
         content = f.read()
         if not content:
             m = {}
         else:
             m = json.loads(content)
             
-    json_data = m.copy()
+    json_data = m.copy() 
     
     # update papers in each keywords         
     for data in data_all:
@@ -83,8 +83,8 @@ def update_json_file(filename, data_all):
             else:
                 json_data[keyword] = papers
  
-    with open(filename, "w", encoding='utf-8') as f:
-        json.dump(json_data, f, ensure_ascii=False, indent=4)
+    with open(filename,"w") as f:
+        json.dump(json_data,f)
     
 def json_to_md(filename):
     """
@@ -94,9 +94,9 @@ def json_to_md(filename):
     
     DateNow = datetime.date.today()
     DateNow = str(DateNow)
-    DateNow = DateNow.replace('-', '.')
+    DateNow = DateNow.replace('-','.')
     
-    with open(filename, "r", encoding='utf-8') as f:
+    with open(filename,"r") as f:
         content = f.read()
         if not content:
             data = {}
@@ -106,11 +106,11 @@ def json_to_md(filename):
     md_filename = "README.md"  
       
     # clean README.md if daily already exist else create it
-    with open(md_filename, "w+", encoding='utf-8') as f:
+    with open(md_filename,"w+") as f:
         pass
  
     # write data into README.md
-    with open(md_filename, "a+", encoding='utf-8') as f:
+    with open(md_filename,"a+") as f:
   
         f.write("## Updated on " + DateNow + "\n\n")
         
@@ -124,7 +124,7 @@ def json_to_md(filename):
             # sort papers by date
             day_content = sort_papers(day_content)
         
-            for _, v in day_content.items():
+            for _,v in day_content.items():
                 if v is not None:
                     f.write(v)
  
@@ -137,19 +137,19 @@ if __name__ == "__main__":
     keywords = dict()
     keywords["SLAM"] = "SLAM"
  
-    for topic, keyword in keywords.items():
+    for topic,keyword in keywords.items():
  
         print("Keyword: " + topic)
-        data = get_daily_papers(topic, query=keyword, max_results=10)
+        data = get_daily_papers(topic, query = keyword, max_results = 10)
         data_collector.append(data)
         print("\n")
  
     # update README.md file
     json_file = "cv-arxiv-daily.json"
-    if not os.path.exists(json_file):
-        with open(json_file, 'w', encoding='utf-8') as a:
+    if ~os.path.exists(json_file):
+        with open(json_file,'w')as a:
             print("create " + json_file)
     # update json data
-    update_json_file(json_file, data_collector)
+    update_json_file(json_file,data_collector)
     # json data to markdown
     json_to_md(json_file)
